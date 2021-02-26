aa847c6a38e36098646a4eccbfff0478d40938b7
macgeeker <jamie@macgeeker.com>
2021-02-26 10:16:10 +0800

    ga id

--- docs/_gitbook_plugin_analytics.js
+++ docs/_gitbook_plugin_analytics.js
@@ -1,9 +1,9 @@
-if ('190628986') {
+if ('G-7713C0WZEV') {
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	ga('create', '190628986', 'auto');
+	ga('create', 'G-7713C0WZEV', 'auto');
 	ga('send', 'pageview');
 }
